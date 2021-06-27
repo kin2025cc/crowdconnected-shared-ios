95c4af4c065a6370496637b756799630073fb0ad
Robert Tanase <robert.tanase97@gmail.com>
2021-06-27 16:09:10 +0100

    Add SPM distribution support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.4
+import PackageDescription
+
+let package = Package(
+    name: "CrowdConnectedShared",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CrowdConnectedShared",
+            targets: ["CrowdConnectedShared"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CrowdConnectedShared",
+            path: "CrowdConnectedShared.xcframework"
+        )
+    ]
+)
