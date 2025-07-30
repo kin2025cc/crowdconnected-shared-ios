035ff92dd33b48fe847e4f80871fa3767fa74cac
CrowdConnected CI <ci@crowdconnected.com>
2025-07-30 20:07:30 +0100

    Version 1.0.0

--- CrowdConnectedShared.xcframework/ios-arm64/CrowdConnectedShared.framework/Headers/CrowdConnectedShared.h
+++
@@ -1,9 +0,0 @@
-//  Created by TCode on 11/03/2021.
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for CrowdConnectedShared.
-FOUNDATION_EXPORT double CrowdConnectedSharedVersionNumber;
-
-//! Project version string for CrowdConnectedShared.
-FOUNDATION_EXPORT const unsigned char CrowdConnectedSharedVersionString[];
